d30c044c8c0a2064913bc5db03da47039f58add8
metaory <metaory@gmail.com>
2022-03-20 08:31:32 +0800

    updated demo

--- src/demo.sh
+++ src/demo.sh
@@ -8,7 +8,17 @@ __section () {
   pastel paint -n $1 $(printf "%0.s█" $(seq 1 "${third}"))
   pastel paint -n $1 ''
 }
+__fill ()  { printf "%0.s${2:- }" $(seq 1 "${1:-0}"); }
+
+__print_logo () {
+  pastel paint -n $SBG "▄▄█"
+  pastel paint -n $WBG "██"
+  pastel paint -n $EBG "█▀▀"
+  echo
+}
+
 __print_head () {
+
   __section $SBG
   __section $WBG
   __section $EBG
@@ -18,13 +28,15 @@ __print_head () {
   echo -e '\n\n'
 }
 
-__print_hexes () {
+
+__print_row () {
+  local char=$1
+  shift
   while [ "${1:-}" ]; do
     local tc="$(pastel textcolor "${!1}")"
     local cc="${!1}"
-    # pastel paint -n -o "${!1}" "${tc}" " ${1} "
-    pastel paint -n -o "#000" "${cc}" " ${cc:(-6)} "
-    # printf ' '
+    [[ "$char" == ' ' ]] && char=" ${cc:(-6)} "
+    pastel paint -n -o "#000" "${cc}" " $char "
     shift
   done
   echo
@@ -33,11 +45,20 @@ __print_hexes () {
 demo () {
   __print_head
 
-  __print_hexes {S,W,E}BG
-  __print_hexes CX{1..6}
-  __print_hexes C0{1..6}
-  __print_hexes CY{1..6}
-  __print_hexes SK{0..9}
-  __print_hexes WK{0..9}
-  __print_hexes EK{0..9}
+  __fill 9;  __print_logo
+
+  __fill 10; __print_row ● {S,W,E}BG
+  __fill 6;  __print_row ● CX{1..6}
+  __fill 6;  __print_row ● C0{1..6}
+  __fill 6;  __print_row ● CY{1..6}
+  __fill 1;  __print_row ● SK{0..9}
+  __fill 1;  __print_row ● WK{0..9}
+  __fill 1;  __print_row ● EK{0..9}
+
+  __print_row " " {S,W,E}BG
+  __print_row " " CX{1..6}
+  __print_row " " C0{1..6}
+  __print_row " " CY{1..6}
+  echo
+  for i in {0..9}; do __print_row " " SK${i} WK${i} EK${i}; done
 }
